00ac0217fbf78eb726a7aaa0b4a0338e2a9ea80b
Gregory Corron <gcorron@fastmail.com>
2020-04-16 10:17:07 -0700

    Job screen

--- TimeCard.Domain/Job.cs
+++ TimeCard.Domain/Job.cs
@@ -3,6 +3,7 @@ namespace TimeCard.Domain
 {
     public class Job
     {
+        private DateTime BaselineDate = new DateTime(2018, 12, 22);
         public int JobId { get; set; }
         public int ClientId { get; set; }
         public int ProjectId { get; set; }
@@ -11,5 +12,32 @@ namespace TimeCard.Domain
         public string Client { get; set; }
         public string Project { get; set; }
         public bool Active { get; set; }
+        public decimal StartDay { get; set; }
+        public DateTime? StartDate
+        {
+            get
+            {
+                if (StartDay == 0)
+                {
+                    return null;
+                }
+
+                int cycle = (int)Decimal.Floor(StartDay);
+                return BaselineDate.AddDays((double)(cycle * 14 + (StartDay - cycle) * 100));
+            }
+
+            set
+            {
+                if (value == null || value < BaselineDate)
+                {
+                    StartDay = 0;
+                }
+                else
+                {
+                    int days = (value - BaselineDate).Value.Days;
+                    StartDay = days / 14 + (days % 14) * (decimal)0.01;
+                }
+            }
+        }
     }
 }
---
+++ TimeCard/ViewModels/JobViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TimeCard.Domain;
+
+namespace TimeCard.ViewModels
+{
+    public class JobViewModel
+    {
+        public int ContractorId { get; set; }
+        public IEnumerable<Job> Jobs { get; set; }
+    }
+}
